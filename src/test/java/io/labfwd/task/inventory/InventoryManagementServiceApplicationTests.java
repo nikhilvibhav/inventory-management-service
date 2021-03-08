f9f8277a4786fd87caab08fb8e2b407e0cb4fb4d
Nikhil Vibhav <nikhil92ind@gmail.com>
2021-03-08 19:38:35 +0530

    Initial commit - Skeleton Service

---
+++ src/test/java/io/labfwd/task/inventory/InventoryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.labfwd.task.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
